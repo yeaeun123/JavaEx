1951389248d4f35b42125217e08f4402ebff1d93
하이미디어학원 <@DESKTOP-KN46D94>
2024-03-21 14:00:10 +0900

    240321

---
+++ src/com/javaex/basic/reftypes/ArrayCopyEx.java
@@ -0,0 +1,59 @@
+package com.javaex.basic.reftypes;
+
+import java.util.Arrays;
+
+public class ArrayCopyEx {
+	//배열은 크기 변경 불가
+	// 공간을 늘리고자 한다면 새 배열을 생성 후 복사를 해야한다.
+	public static void main(String[] args) {
+		// For 문을 이용한 배열 복사
+//		arraCopyByFor();
+		// System을 이용한 배열 복사
+		arrayCopyBySystem();
+	}
+	
+	public static void arraCopyByFor() {
+		 int scores[] = {1, 2, 3};	//.length ==3, index=0,1,2
+		 for (int i = 0; i < scores.length; i++) {
+			 System.out.print(scores[i] + "\t");
+		 }
+		 System.out.println();
+		 
+		 // 새 공간을 만들어 복사
+		 int target[] = new int[10];
+		 
+		 for (int i =0; i < scores.length; i++) {
+			 target[i] = scores[i];
+		 }
+		 // 배열 유틸리티 클래스를 이용한 출력
+		 System.out.println(Arrays.toString(target));
+	}
+	
+	public static void arrayCopyBySystem() {
+		int source[] = {1,2,3};
+		int target[] = new int[10];
+		
+		System.out.println("source: "
+		+ Arrays.toString(source));
+		
+		// 배열 복제
+		System.arraycopy(source, //원본배열,
+				0, //복사 시작 인덱스(원본)
+				target,//복사 대상 배열
+				3,// 대상 배열의 시작 인덱스
+				source.length); //복사할 길이
+		
+		System.out.println("target:"
+						+ Arrays.toString(target));
+		
+		// Enhanced For
+		// for(추출된 데이터: 원본 배열 혹은 컬렉션)
+		for (int value: target) {
+			System.out.print(value +"\t");	
+		}
+		System.out.println();
+		
+				
+	}
+
+}
