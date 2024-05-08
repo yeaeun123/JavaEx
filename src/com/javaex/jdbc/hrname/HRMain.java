386b85b84994f8dd0a9736c71f2ddb8726ee9428
신예은 <yeaeun0125@gmail.com>
2024-05-08 18:07:23 +0900

    20240508오후

--- src/com/javaex/jdbc/hrname/HRMain.java
+++ src/com/javaex/jdbc/hrname/HRMain.java
@@ -7,6 +7,16 @@ import com.javaex.jdbc.daopractice.EmployeeDAO;
 import com.javaex.jdbc.daopractice.EmployeeDAOImplOracle;
 import com.javaex.jdbc.daopractice.EmployeeVO;
 
+
+/*
+ *  [실습] 사원 정보 검색 프로그램 (DAO 버전)
+: 클래스 Scanner를 사용하여 사원 이름을 입력 받아 사원 정보를 검색하는 프로
+그램을 작성해 봅시다
+ 부분 이름 검색이 가능해야 합니다
+ 성, 이름 컬럼에 대해 OR 검색이 되어야 합니다
+ 출력 사원 정보는 이름 성, Email, 전화번호, 입사일, 월급 입니다
+ HRApp 프로젝트에서 HRMain Class에 main 메서드가 있어야 합니다
+ */
 public class HRMain {
 
 	public static void main(String[] args) {
